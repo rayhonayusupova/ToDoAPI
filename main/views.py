51e63f1de204a78174de07cc916f608cfce48bea
Rayhona <yusupovar686@gmail.com>
2026-01-05 00:30:17 +0500

    Todo API using DRF generic views

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
